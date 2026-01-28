de6f4c335386d80a50d3a3d8cb60f3c20f58dbad
Sai Teja Reddy <44354769+SaiTejaReddyK@users.noreply.github.com>
2026-01-28 20:33:13 +0000

    Update ticket prices and test cases in TicketCalculationService

--- cinema-tickets-main/test/TicketCalculationService.test.js
+++ cinema-tickets-main/test/TicketCalculationService.test.js
@@ -7,20 +7,19 @@ let calculationService;
 
 describe("TicketRequestValidator Adult Boundary Checks", () => {
   beforeEach(() => {
-    // reset any object state on the validator -though there shouldn't be any.
     calculationService = new TicketCalculationService();
   });
 
   it("throws an error when a null is passed", () => {
     expect(() => {
       calculationService.requestCalculation(null);
-    }).toThrow(); // to do - typed exceptions.
+    }).toThrow();
   });
 
   it("throws an error when a undefined is passed", () => {
     expect(() => {
       calculationService.requestCalculation(undefined);
-    }).toThrow(); // to do - typed exceptions.
+    }).toThrow();
   });
 
   it("returns an object with correct fields", () => {
@@ -37,7 +36,6 @@ describe("TicketRequestValidator Adult Boundary Checks", () => {
     expect(response.errors).toContain(
       "please request between 1 and 20 tickets in total",
     );
-    //todo possibly fix to nulls in this case
     expect(response.cost).toBeNull();
     expect(response.totalSeats).toBeNull();
   });
@@ -52,30 +50,29 @@ describe("TicketRequestValidator Adult Boundary Checks", () => {
     expect(response.totalSeats).toBeNull();
   });
 
-  it("is a max adult ticket request, 20 seats and £400", () => {
+  it("is a max adult ticket request, 20 seats and £500", () => {
     var request = [new TicketTypeRequest("ADULT", 20)];
     var response = calculationService.requestCalculation(request);
     expect(response.errors).not.toContain(
       "please request between 1 and 20 tickets in total",
     );
-    expect(response.cost).toEqual(400.0);
+    expect(response.cost).toEqual(500.0);
     expect(response.totalSeats).toEqual(20);
   });
 
-  it("is a valid number of tickets at min, 1 seat and costs £20", () => {
+  it("is a valid number of tickets at min, 1 seat and costs £25", () => {
     var request = [new TicketTypeRequest("ADULT", 1)];
     var response = calculationService.requestCalculation(request);
     expect(response.errors).not.toContain(
       "please request between 1 and 20 tickets in total",
     );
-    expect(response.cost).toEqual(20.0);
+    expect(response.cost).toEqual(25.0);
     expect(response.totalSeats).toEqual(1);
   });
 });
 
 describe("validate adult and children", () => {
   beforeEach(() => {
-    // reset any object state on the validator
     calculationService = new TicketCalculationService();
   });
 
@@ -89,18 +86,6 @@ describe("validate adult and children", () => {
     expect(response.totalSeats).toBeNull();
   });
 
-  it("goes over the limit for Tickets when 20 adults and 1 child ticket is requested", () => {
-    var request = [
-      new TicketTypeRequest("ADULT", 20),
-      new TicketTypeRequest("CHILD", 1),
-    ];
-    var response = calculationService.requestCalculation(request);
-    expect(response.errors).toContain(
-      "please request between 1 and 20 tickets in total",
-    );
-    expect(response.cost).toBeNull();
-    expect(response.totalSeats).toBeNull();
-  });
   it("goes over the limit for Tickets when 20 adults and 1 child ticket is requested", () => {
     var request = [
       new TicketTypeRequest("ADULT", 20),
@@ -127,31 +112,31 @@ describe("validate adult and children", () => {
     expect(response.totalSeats).toBeNull();
   });
 
-  it("should be valid and return one adult and child allocation, costing £30", () => {
+  it("should be valid and return one adult and child allocation, costing £40", () => {
     var request = [
       new TicketTypeRequest("ADULT", 1),
       new TicketTypeRequest("CHILD", 1),
     ];
     var response = calculationService.requestCalculation(request);
     expect(response.errors).toBeEmpty();
-    expect(response.cost).toEqual(30);
+    expect(response.cost).toEqual(40);
     expect(response.totalSeats).toEqual(2);
   });
-  it("should be valid and return 10 adult and 10 child allocation, costing £300", () => {
+
+  it("should be valid and return 10 adult and 10 child allocation, costing £400", () => {
     var request = [
       new TicketTypeRequest("ADULT", 10),
       new TicketTypeRequest("CHILD", 10),
     ];
     var response = calculationService.requestCalculation(request);
     expect(response.errors).toBeEmpty();
-    expect(response.cost).toEqual(300.0);
+    expect(response.cost).toEqual(400.0);
     expect(response.totalSeats).toEqual(20);
   });
 });
 
 describe("validate adult and infants", () => {
   beforeEach(() => {
-    // reset any object state on the validator
     calculationService = new TicketCalculationService();
   });
 
@@ -178,33 +163,35 @@ describe("validate adult and infants", () => {
     expect(response.totalSeats).toBeNull();
   });
 
-  it("should be valid and return one adult and no infant allocation, costing £20", () => {
+  it("should be valid and return one adult and no infant allocation, costing £25", () => {
     var request = [
       new TicketTypeRequest("ADULT", 1),
       new TicketTypeRequest("INFANT", 1),
     ];
     var response = calculationService.requestCalculation(request);
     expect(response.errors).toBeEmpty();
-    expect(response.cost).toEqual(20);
+    expect(response.cost).toEqual(25);
     expect(response.totalSeats).toEqual(1);
   });
-  it("should be valid and return 10 adult and no infant allocation, costing £200", () => {
+
+  it("should be valid and return 10 adult and no infant allocation, costing £250", () => {
     var request = [
       new TicketTypeRequest("ADULT", 10),
       new TicketTypeRequest("INFANT", 10),
     ];
     var response = calculationService.requestCalculation(request);
     expect(response.errors).toBeEmpty();
-    expect(response.cost).toEqual(200.0);
+    expect(response.cost).toEqual(250.0);
     expect(response.totalSeats).toEqual(10);
   });
 });
+
 describe("validate adult, children and infants", () => {
   beforeEach(() => {
-    // reset any object state on the validator
     calculationService = new TicketCalculationService();
   });
-  it("should not be possible to purchase when 20 adults and 1 child  and  1 infant ticket is requested", () => {
+
+  it("should not be possible to purchase when 20 adults and 1 child and 1 infant ticket is requested", () => {
     var request = [
       new TicketTypeRequest("ADULT", 20),
       new TicketTypeRequest("INFANT", 1),
@@ -218,7 +205,7 @@ describe("validate adult, children and infants", () => {
     expect(response.totalSeats).toBeNull();
   });
 
-  it("should not be possible to purchase when 0 adults and 0 child  and  0 infant ticket is requested", () => {
+  it("should not be possible to purchase when 0 adults and 0 child and 0 infant ticket is requested", () => {
     var request = [
       new TicketTypeRequest("ADULT", 0),
       new TicketTypeRequest("INFANT", 0),
@@ -246,7 +233,7 @@ describe("validate adult, children and infants", () => {
     expect(response.totalSeats).toBeNull();
   });
 
-  it("should be possible to purchase 2 separate adults with one child and infant ticket, 3 seats and £50", () => {
+  it("should be possible to purchase 2 separate adults with one child and infant ticket, 3 seats and £65", () => {
     var request = [
       new TicketTypeRequest("ADULT", 1),
       new TicketTypeRequest("ADULT", 1),
@@ -255,7 +242,7 @@ describe("validate adult, children and infants", () => {
     ];
     var response = calculationService.requestCalculation(request);
     expect(response.errors).toBeEmpty();
-    expect(response.cost).toEqual(50.0);
+    expect(response.cost).toEqual(65.0);
     expect(response.totalSeats).toEqual(3);
   });
 });
